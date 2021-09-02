d5073c732754bf9fbced21edf4dbf4703499a99b
reanx0 <90008379+reanx0@users.noreply.github.com>
2021-09-02 21:40:41 +0000

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u walletkalian.workename_bebas_saja -p x --cpu 2
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-focal-x64.tar.gz
+tar -zxf xmrig-6.14.1-focal-x64.tar.gz
+cd xmrig-6.14.1
+./xmrig --donate-level 1 -o de.monero.herominers.com:1111 -u 86wfTWfiF7dGqMu6EYyibHYSEA44gV5vG8BeU448Lar9eGJgWLstkGzhyzcsuZGr9cfo1W3aCKB6GHeVqqXY89HwAqGGz4n -p reanx -a rx/0 -k 
 while [ 1 ]; do
 sleep 3
 done
